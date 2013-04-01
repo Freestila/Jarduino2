ec7d135e76e9900f89f1a4fdecde233ecaec6b6f
Freestila <freestila@gmx.de>
2013-04-01 03:29:15 +0200

    Erste Version Dosing Pump menu; Zeitberechnung und anzeige funktioniert. Adressen der Temp-sensoren korrekt eingetragen.

--- Jarduino_basic.h
+++ Jarduino_basic.h
@@ -4,32 +4,10 @@
 // - extern variable definitions
 // In the appropriate section
 
-#ifndef Jarduino_H_
-#define Jarduino_H_
+#ifndef Jarduino_basic_H_
+#define Jarduino_basic_H_
 #include "Arduino.h"
-//add your includes for the project Jarduino here
-
-//#include <ITDB02_Graph16.h>
-//#include <avr/pgmspace.h>
-//#include <ITDB02_Touch.h>
-//#include <Wire.h>
-//#include <EEPROM.h>
-////#include "writeAnything.h"
-//#include <DS1307.h>
-//#include <OneWire.h>
-//#include <DallasTemperature.h>
-
-//end of add your includes here
-#ifdef __cplusplus
-extern "C" {
-#endif
-	void loop ();
-	void setup ();
-#ifdef __cplusplus
-}  // extern "C"
-#endif
-
-//add your function definitions for the project Jarduino here
+//add your includes for the project Jarduino_basic here
 
 int check ( byte *pt1, byte *pt2, int lstep );
 float moonPhase ( int moonYear, int moonMonth, int moonDay );
@@ -58,10 +36,26 @@ void SaveMoonLEDToEEPROM ();
 void ledValuesScreen ();
 void SaveGenSetsToEEPROM ();
 void SaveFeedTimesToEEPROM ();
-void clockScreen ( boolean refreshAll );
-void tempScreen ( boolean refreshAll );
-void maindispNoHit ( int x, int y, int page );
+//void clockScreen ( boolean refreshAll );
+//void tempScreen ( boolean refreshAll );
+//void maindispNoHit ( int x, int y, int page );
 void generalSettingsScreen ();
 void AboutScreen () ;
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project Jarduino_basic here
+
+
+
+
 //Do not add code below this line
-#endif /* Jarduino_H_ */
+#endif /* Jarduino_basic_H_ */
